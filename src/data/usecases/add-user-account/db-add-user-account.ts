9766a4177c85057198caa0fb128ccd31ea82e726
Samuel Goulart <62961331+SamuelGoulart@users.noreply.github.com>
2022-04-19 09:15:48 -0300

    feat: addUserAccountModel

---
+++ src/data/usecases/add-user-account/db-add-user-account.ts
@@ -0,0 +1,19 @@
+import { AddUserAccount, AddUserAccountModel, AccountModel, Hasher, AddAccountRepository} from "./db-add-user-account-protocols";
+
+export class DbAddAccount implements AddUserAccount {
+    private readonly hasher: Hasher
+    private readonly addAccountRepository: AddAccountRepository
+
+    constructor(hasher: Hasher, addAccountRepository: AddAccountRepository) {
+        this.hasher = hasher
+        this.addAccountRepository = addAccountRepository
+    }
+    async add (accountData: AddUserAccountModel): Promise<AccountModel> {
+
+        const password = Math.random().toString(36).slice(-10)
+        const hashedPassword = await this.hasher.hash(password)
+        const account = await this.addAccountRepository.add(Object.assign({}, accountData, { password: hashedPassword }))
+        return account
+    }
+
+}
