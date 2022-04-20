3f7422bb15396ba87d9bde93a0b21b2b552af8ff
Samuel Goulart <62961331+SamuelGoulart@users.noreply.github.com>
2022-04-20 10:35:00 -0300

    refactor: server startup improvement

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,10 +1,11 @@
 import connection from '../infra/db/mysql/helpers/connect'
 import app from './config/app'
 
-connection()
-app.listen(5050, () => console.log('Server running at http://localhost:5050'))
-
-// connection().then(async () => {
-//   const app = (await import('./config/app')).default
-//   app.listen(5050, () => console.log('Server running at http://localhost:5050'))
-// }).catch(console.error)
+void (async () => {
+  try {
+    await connection.raw('select 1')
+    app.listen(5050, () => console.log('Server running at http://localhost:5050'))
+  } catch (error) {
+    console.error(error)
+  }
+})()
